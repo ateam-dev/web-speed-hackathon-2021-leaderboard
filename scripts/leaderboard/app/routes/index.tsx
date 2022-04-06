710ac7ed3b255e559dfef1e26e1c487ac2970238
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-04-06 11:25:27 +0900

    Install chakra-ui

--- scripts/leaderboard/app/routes/index.tsx
+++ scripts/leaderboard/app/routes/index.tsx
@@ -1,32 +1,13 @@
-export default function Index() {
+
+
+import { Box } from "@chakra-ui/react";
+
+const Index = () => {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Box bg="tomato" w="100%" p={4} color="white">
+      Hello World!
+    </Box>
   );
-}
+};
+
+export default Index;
