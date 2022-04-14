e119d3cde46cf7639c9cb3ff2decfa2fbf052750
AijiUejima <aiji42@gmail.com>
2022-04-14 15:45:04 +0900

    Fix supabaseClient for server and codenized RLS (#67)

--- scripts/leaderboard/app/libs/supabase.server.ts
+++ scripts/leaderboard/app/libs/supabase.server.ts
@@ -1,5 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_API_KEY, {
-  fetch: fetch.bind(globalThis),
+  fetch: (url, inits) => {
+    if (!url.toString().includes("/rest/v1")) return fetch(url, inits);
+    return fetch(url, {
+      ...inits,
+      ...(inits?.headers && {
+        headers: {
+          ...inits.headers,
+          Authorization: `Bearer ${SUPABASE_API_KEY}`,
+        },
+      }),
+    });
+  },
 });
