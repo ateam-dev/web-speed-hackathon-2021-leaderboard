41d09b6c074a23b701336be057a734d638d032ad
chimame <rito.tamata@gmail.com>
2022-04-15 14:22:26 +0900

    Fix start measure server for development

--- scripts/measure/docker-entrypoint.sh
+++ scripts/measure/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 yarn install
-yarn workspace @web-speed-hackathon/scoring run build:watch
-yarn workspace @web-speed-hackathon/vrt run build:watch
+yarn workspace @web-speed-hackathon/scoring run build:watch &
+yarn workspace @web-speed-hackathon/vrt run build:watch &
 exec "$@"
