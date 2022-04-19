bd7fb636371d36dc50a3064e1de85dee299f0e82
chimame <rito.tamata@gmail.com>
2022-04-19 19:01:27 +0900

    Modify scoring when scoring failed (#88)

--- scripts/scoring/src/index.ts
+++ scripts/scoring/src/index.ts
@@ -7,15 +7,9 @@ export async function main(id: string, url: string) {
     url,
   };
 
-  //const targetPaths = JSON.parse(process.env['WSH_SCORING_TARGET_PATHS'] as string);
   const targetPaths = ['/'];
 
   const result = await scoring(competitor, targetPaths);
-
-  if ('error' in result.result) {
-    throw result.result.error;
-  }
-  console.log(`::set-output name=export::${JSON.stringify(result)}`);
 
   return result;
 }
